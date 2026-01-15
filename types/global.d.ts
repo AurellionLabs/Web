92acba9fa1d6788901853157df025abadeb6db90
AureliusEth <matthewpinnock.mp@gmail.com>
2026-01-15 16:15:38 +0000

    fix: use Eip1193Provider type for window.ethereum

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,12 +1,8 @@
+import type { Eip1193Provider } from 'ethers';
+
 declare global {
   interface Window {
-    ethereum?: {
-      request: (args: { method: string; params?: any[] }) => Promise<any>;
-      on: (event: string, callback: (...args: any[]) => void) => void;
-      removeListener: (
-        event: string,
-        callback: (...args: any[]) => void,
-      ) => void;
+    ethereum?: Eip1193Provider & {
       isMetaMask?: boolean;
     };
   }
